27e86ab70417c97ccf7dd5168963d993c40d1043
anders elo <anders@kaerus.com>
2013-02-28 13:23:34 +0100

    included tests and fixes

--- emitter.js
+++ emitter.js
@@ -22,21 +22,44 @@ try{root = global} catch(e){try {root = window} catch(e){root = this}};
 (function () {
     "use strict"
 
-    function Emitter() {
-        if(!(this instanceof Emitter)) {
-            return new Emitter();
-        }  
+    function Emitter(obj) {
+        /* Emitter mixin */
+        if(obj) {
+            for(var key in Emitter.prototype) {
+                obj[key] = Emitter.prototype[key];
+            }
+            obj._events = {};
+            return obj;
+        }
 
-        Object.defineProperty(this,'_events',{
-            enumerable: false,
-            value: {}
-        });
+        if(!(this instanceof Emitter)) {
+            return new Emitter;
+        }
+
+        this._events = {};  
+    }
+
+    Emitter.prototype.hasEvent = function(event) {
+        return !!this._events[event];
+    }
+
+    Emitter.prototype.listeners = function(event) {
+        var handlers = this._events[event];
+
+        if(!handlers) return [];
+        if(!Array.isArray(handlers)) return [handlers];
+        
+        return handlers;
+    }
+
+    Emitter.prototype.hasListeners = function(event) {
+        return !!this._events[event];
     }
 
     Emitter.prototype.on = function(event,handler) {
-        var e;
+        var e = this._events[event];
         
-        if(!(e = this._events[event])) {
+        if(!e) {
             this._events[event] = handler;
         } else if(!Array.isArray(e)) {
             if(e !== handler) this._events[event] = [e].concat(handler);  
@@ -48,81 +71,94 @@ try{root = global} catch(e){try {root = window} catch(e){root = this}};
     }
 
     Emitter.prototype.before = function(event,handler) {
-         return this.on(event,{before:handler});
+         return this.on(event,{_before:handler});
     }
 
     Emitter.prototype.after = function(event,handler) {
-         return this.on(event,{after:handler});
-    }
-
-    Emitter.prototype.hasEvent = function(name) {
-        return !!this._events[name];
+         return this.on(event,{_after:handler});
     }
 
     Emitter.prototype.off = function(event,handler) {
-        if(!this._events[event]) return;
+        handler = handler ? handler._off || handler : null;
+        
+        if(event){ 
+            if(!this._events[event]) return;
 
-        if(!handler || !Array.isArray(this._events[event])) {
-            if(!handler || this._events[event] === handler)
-                delete this._events[event];
-        } else {
-            /* remove matching handlers */
-            this._events[event] = this._events[event].filter(function(f){
-                return f !== handler && f.before !== handler && f.after !== handler
-            });
-            /* undefines event when no handler is attached */
-            /* or unwraps handler array on single handler. */
-            if(!this._events[event].length) this._events[event] = undefined;
-            else if(this._events[event].length === 1) 
-                this._events[event] = e[0];
-        }   
+            if(!handler) {
+                this._events[event] = undefined;
+            }
+            else if(!Array.isArray(this._events[event])){
+                if(this._events[event] === handler)
+                    this._events[event] = undefined;    
+            } else {
+
+                this._events[event] = this._events[event].filter(function(f) {
+                    return f !== handler && f._before !== handler && f._after !== handler
+                });
+                /* undefines event when no handler is attached */
+                /* or unwraps handler array on single handler. */
+                if(!this._events[event].length) this._events[event] = undefined;
+                else if(this._events[event].length === 1) 
+                    this._events[event] = this._events[event][0];
+            } 
+        }
+        else {
+            if(!handler) this._events = {};
+            else {
+                var events = Object.keys(this._events);
+                for(var i = 0, l = events.length; i < l; i++)
+                    this.off(events[i],handler);
+            }
+        } 
 
         return this;
     }
 
     Emitter.prototype.emit = function(event) {
-        var e;
+        var handlers = this._events[event];
 
-        if(!(e = this._events[event])) return;
+        if(!handlers) return;   
 
         var args = Array.prototype.slice.call(arguments,1),
-            handler, before, after = [], stop = false;
+            handler, before, after = [], done = false;
 
-        if(!Array.isArray(e)) {
-            e.apply(null,args);
+        if(!Array.isArray(handlers)) {
+            handlers.apply(null,args);
 
             return this;
         }
 
-        before = e.filter(function(f){return f.before});
+        before = handlers.filter(function(f){return f._before});
 
         before.forEach(function(o){
-            if(!stop) {
-                if(o.before.apply(null,args) === false ) {
-                    stop = true;
+            if(!done) {
+                if(o._before.apply(null,args) === false ) {
+                    done = true;
                 }
             }
         });
 
-        if(stop) return this;
+        if(done) return this;
 
-        for (var i = 0, l = e.length; i < l; i++) {
-            handler = e[i];
+        for (var i = 0, l = handlers.length; i < l; i++) {
+            handler = handlers[i];
             if(typeof handler === 'function' ) {
                 /* stop propagation on false */
                 if( handler.apply(null,args) === false ) {
-                    stop = true;
+                    done = true;
                     break;
                 }
-            } else if(typeof handler === 'object' && handler.after) {
-                after[after.length] = handler.after;
+            } else if(typeof handler === 'object' && handler._after) {
+                after[after.length] = handler._after;
             } /* silently ignore invalid handlers */   
         }
 
+        if(done) return this;
+
         after.forEach(function(handler){
-            if(!stop) {
+            if(!done) {
                 if(handler.apply(null,args) === false ) {
-                    stop = true;
+                    done = true;
                 }
             }  
         });  
@@ -133,10 +169,13 @@ try{root = global} catch(e){try {root = window} catch(e){root = this}};
     Emitter.prototype.once = function(event,handler) {
         var self = this;
 
-        this.on(event, function h() {
-            self.off(event, h);
+        function once() {
+            self.off(event, once);
             handler.apply(null, arguments);
-        });
+        }
+
+        this.on(event, once);
+        handler._off = once;
 
         return this;
     }
